52550b8d5d37738de52883ebbbd2284033004d79
Dani <Daniaalshehri@outlook.com>
2024-08-12 03:36:43 +0300

    ''

--- app.py
+++ app.py
@@ -4,33 +4,31 @@ st.title('!لا توجد فرص وظيفية بعد التخرج')
 
 st.image("img.gif")
 
-st.markdown("""
-دائمًا نسمع أنه ما فيه وظائف لحديثي التخرج، وأنه ما فيه وظائف إلا بالواسطة، فلا تتعب نفسك في الدراسة. خلينا نشوف إذا كان كلامهم صحيحًا أو لا.
-أخذت بيانات من منصة "جدارات" وحللت بيانات آخر سنتين للتعرف على سوق العمل.
-""")
-
-
-
 
 st.write("<p style='text-align: right;'> دائمًا نسمع أنه ما فيه وظائف لحديثي التخرج، وأنه ما فيه وظائف إلا بالواسطة، فلا تتعب نفسك في الدراسة. </p>", unsafe_allow_html=True)
 st.write("<p style='text-align: right;'> خلينا نشوف إذا كان كلامهم صحيحًا أو لا. </p>", unsafe_allow_html=True)
 st.write("<p style='text-align: right;'> أخذت بيانات من منصة جدارات وحللت بيانات آخر سنتين للتعرف على سوق العمل. </p>", unsafe_allow_html=True)
-
+st.write("    ")
 st.image("img1.png", use_column_width=True)
 st.write("<p style='text-align: right;'> أولاً، خلينا نشوف أي المناطق تعرض وظائف أكثر للخريجين. وكما هو موضح في الرسمة، نجد أن منطقة الرياض هي الأعلى. نقدر نقول إن منطقتي الرياض ومكة توفران فرصًا أكثر لحديثي التخرج </p>", unsafe_allow_html=True)
-
-st.image("img2.png", use_column_width=True)
-
+st.write("    ")
+st.write("    ")
 st.write("<p style='text-align: right;'>طيب، ما هي أكثر الوظائف طلبًا؟  </p>", unsafe_allow_html=True)
+st.image("img2.png", use_column_width=True)
 st.write("<p style='text-align: right;'> بائع هو أكثر وظيفة طلبًا، يليه محاسب. </p>", unsafe_allow_html=True)
-
-st.image("img3.png", use_column_width=True)
+st.write("    ")
+st.write("    ")
 st.write("<p style='text-align: right;'> والآن، دعونا نشوف متوسط رواتب حديثي التخرج. راتب 4 آلاف قد يكون قليلًا، لكن هذه مجرد بداية، ومع إضافة الخبرة والمهارات لاحقًا، سيزيد الراتب إن شاء الله (بداية الغيث قطرة). </p>", unsafe_allow_html=True)
+st.image("img3.png", use_column_width=True)
 
+st.write("    ")
+st.write("    ")
 st.write("<p style='text-align: right;'> هل لديك فضول لمعرفة ما هو أعلى راتب؟   </p>", unsafe_allow_html=True)
 st.image("img4.png", use_column_width=True)
 st.write("<p style='text-align: right;'>  هناك تنوع في التخصصات في القائمة، ولكن الطبيب النفسي والمحلل المبرمج لهم أعلى متوسط راتب</p>", unsafe_allow_html=True)
 
+st.write("    ")
+st.write("    ")
 st.write("<p style='text-align: right;'> والآن أثبتنا أن هناك عروضًا وطلبًا لحديثي التخرج، وهناك تنوع في الوظائف المعروضة. لذا، كونوا مطمئنين أنكم طالما تعملون بجد، فستجدون الوظيفة المناسبة. </p>", unsafe_allow_html=True)
 st.write("<p style='text-align: right;'>  </p>", unsafe_allow_html=True)
 st.write("<p style='text-align: right;'>  </p>", unsafe_allow_html=True)
