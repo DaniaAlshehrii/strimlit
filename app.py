acf55edb2fd63585551254620c7994f68e1acf1c
Dani <Daniaalshehri@outlook.com>
2024-08-12 03:04:14 +0300

    'file'

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
 import streamlit as st
 
-st.write("H world")
+st.title('لا توجد فرص وظيفية بعد التخرج !')
+
+st.image("img.gif", use_column_width=True)
+
+
+st.markdown("""
+دائمًا نسمع أنه ما فيه وظائف لحديثي التخرج، وأنه ما فيه وظائف إلا بالواسطة، فلا تتعب نفسك في الدراسة. خلينا نشوف إذا كان كلامهم صحيحًا أو لا.
+أخذت بيانات من منصة "جدارات" وحللت بيانات آخر سنتين للتعرف على سوق العمل.
+""")
+
+st.image("img1.gif", use_column_width=True)
+
+st.markdown("""
+دائمًا نسمع أنه ما فيه وظائف لحديثي التخرج، وأنه ما فيه وظائف إلا بالواسطة، فلا تتعب نفسك في الدراسة. خلينا نشوف إذا كان كلامهم صحيحًا أو لا.
+أخذت بيانات من منصة "جدارات" وحللت بيانات آخر سنتين للتعرف على سوق العمل.
+""")
+
+st.write("<p style='text-align: right;'> دائمًا نسمع أنه ما فيه وظائف لحديثي التخرج، وأنه ما فيه وظائف إلا بالواسطة، فلا تتعب نفسك في الدراسة. خلينا نشوف إذا كان كلامهم صحيحًا أو لادائمًا نسمع أنه ما فيه وظائف لحديثي التخرج، وأنه ما فيه وظائف إلا بالواسطة، فلا تتعب نفسك في الدراسة. خلينا نشوف إذا كان كلامهم صحيحًا أو لا.p>", unsafe_allow_html=True)
+st.write("<p style='text-align: right;'>أخذت بيانات من منصة جدارات وحللت بيانات آخر سنتين للتعرف على سوق العمل. p>", unsafe_allow_html=True)
+st.write("<p style='text-align: right;'> p>", unsafe_allow_html=True)
+st.write("<p style='text-align: right;'> p>", unsafe_allow_html=True)
+st.write("<p style='text-align: right;'> p>", unsafe_allow_html=True)
+st.write("<p style='text-align: right;'> p>", unsafe_allow_html=True)
